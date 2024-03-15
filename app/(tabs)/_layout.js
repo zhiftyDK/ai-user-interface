8559cd6b33725aaf967c6806b0f9aa472082f691
CodeZhifty <juulstausholmoscar@gmail.com>
2024-03-15 17:59:00 +0100

    Fixed font

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,18 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
+import { SplashScreen, Tabs } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFonts, Orbitron_400Regular } from '@expo-google-fonts/orbitron';
 
-export default function TabLayout() {
+export default function TabLayout() {  
     const [name, setName] = useState("Home");
     
+    const [fontsLoaded] = useFonts({
+        Orbitron_400Regular,
+    });
+
     useEffect(() => {
         (async () => {
+            await SplashScreen.preventAutoHideAsync();
             const item = await AsyncStorage.getItem("settings-name");
             setName((item ? item : "Home"));
         })();
     }, []);
+
+    if (!fontsLoaded) {
+        return null;
+    } else {
+        SplashScreen.hideAsync();
+    }
 
     return (
         <Tabs screenOptions={{ tabBarActiveTintColor: '#fff', tabBarStyle: {backgroundColor: "#262833"}, headerStyle: {backgroundColor: "#262833"}, headerTitleStyle: {color: "#fff", fontFamily: "Orbitron_400Regular"}}}>
--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,12 @@
 import { Stack } from 'expo-router/stack';
+import { SplashScreen } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function AppLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+    );
 }
